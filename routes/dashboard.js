1f78ada68cf0c2e66990a597e4c3126d462ea089
Lucas Souleyrasse <you@example.comlucas.souleyrasse@gmail.com>
2021-09-20 18:35:59 +0200

    fixed route

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -6,10 +6,9 @@ router.get("/", authorization, async (req, res) => {
   try {
     //req.user has the payload
     // res.json(req.user);
-    const user = await pool.query(
-      "SELECT first_name FROM users WHERE user_id = $1",
-      [req.user]
-    );
+    const user = await pool.query("SELECT * FROM users WHERE user_id = $1", [
+      req.user,
+    ]);
     res.json(user.rows[0]);
   } catch (err) {
     console.error(err.message);
